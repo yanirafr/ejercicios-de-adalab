09b8e7a136caf2973f0be8e9a00e60b7d2f7e9cd
Yanira <yanirafr@gmail.com>
2020-10-02 11:08:19 +0200

    upload

---
+++ 2.2/2.2-10/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const myAge = document.querySelector(".age");
+const years = parseInt(myAge.innerHTML);
+const daysPerYear = 365;
+const hoursPerDay = 24;
+const totalHours = years * daysPerYear * hoursPerDay;
+
+console.log(totalHours);
---
+++ 2.2/2.2-9/main.js
@@ -0,0 +1,4 @@
+"use strict";
+const name = "Lissandra";
+const paragraph = document.querySelector(".text");
+paragraph.innerHTML = `El nombre de mi compañera es ${name}, y está compuesto por ${name.length} caracteres`;
