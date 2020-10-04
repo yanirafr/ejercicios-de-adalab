8a2ef2b9d1cc168f05486fc10cafc1bd71548681
Yanira <yanirafr@gmail.com>
2020-10-04 20:08:25 +0200

    upload

--- 2.3/2.3-7/main.js
+++ 2.3/2.3-7/main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+let ingredient = "Espinacas";
+
+// aquí tu código Filete con patatas / Merluzita en salsa verde / Espinacas rehogadas
+
+switch (ingredient) {
+  case "Pollo":
+    console.log("Filete con patatas");
+    break;
+  case "Espinacas":
+    console.log("Espinacas rehogadas");
+    break;
+  case "Merluza":
+    console.log("Merluzita en salsa verde");
+    break;
+  default:
+    console.log("Nada en la nevera");
+}
