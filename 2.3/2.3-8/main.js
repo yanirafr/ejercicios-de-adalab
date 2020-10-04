b330b10cc93d04d2af72f5939aa6691caa40d8b9
Yanira <yanirafr@gmail.com>
2020-10-05 00:05:40 +0200

    upload

--- 2.3/2.3-8/main.js
+++ 2.3/2.3-8/main.js
@@ -1,2 +1,11 @@
 "use strict";
-const currentYear = 3;
+const currentYear = parseFloat(3);
+if (currentYear % 4 === 0) {
+  console.log(`Estamos en un año bisiesto`);
+} else if ((currentYear + 1) % 4 === 0) {
+  console.log(`El próximo año bisiesto es el (1 + ${currentYear})`);
+} else if ((currentYear + 2) % 4 === 0) {
+  console.log(`El próximo año bisiesto es el (2 + ${currentYear})`);
+} else if ((currentYear + 3) % 4 === 0) {
+  console.log(`El próximo año bisiesto es el (3 + ${currentYear})`);
+}
