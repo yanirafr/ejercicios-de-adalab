1541a8bd3b66af5ccf1d7b9474125d6ffe54e9f6
Yanira <yanirafr@gmail.com>
2020-10-05 08:33:23 +0200

    solved

--- 2.3/2.3-8/main.js
+++ 2.3/2.3-8/main.js
@@ -1,13 +1,14 @@
 "use strict";
+
 let year = document.querySelector(".year");
 let currentYear = parseFloat(year.innerHTML);
 const leap = document.querySelector(".leap");
 if (currentYear % 4 === 0) {
   leap.innerHTML = "Estamos en un año bisiesto";
 } else if ((currentYear + 1) % 4 === 0) {
-  leap.innerHTML = currentYear + 1;
+  leap.innerHTML = "El próximo año bisiesto es el " + (currentYear + 1);
 } else if ((currentYear + 2) % 4 === 0) {
-  leap.innerHTML = 2 + currentYear;
+  leap.innerHTML = "El próximo año bisiesto es el " + (2 + currentYear);
 } else if ((currentYear + 3) % 4 === 0) {
-  leap.innerHTML = 3 + currentYear;
+  leap.innerHTML = "El próximo año bisiesto es el " + (3 + currentYear);
 }
