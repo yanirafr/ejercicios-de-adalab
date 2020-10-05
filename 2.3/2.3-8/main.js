a8cf492f68add79df27cc54b8a90fe1c232db44f
Yanira <yanirafr@gmail.com>
2020-10-05 11:15:32 +0200

    improve code

--- 2.3/2.3-8/main.js
+++ 2.3/2.3-8/main.js
@@ -6,9 +6,9 @@ const leap = document.querySelector(".leap");
 if (currentYear % 4 === 0) {
   leap.innerHTML = "Estamos en un año bisiesto";
 } else if ((currentYear + 1) % 4 === 0) {
-  leap.innerHTML = `El próximo año bisiesto es el ${currentYear + 1}`;
+  leap.innerHTML = `El próximo año bisiesto es el ${currentYear + 1}.`;
 } else if ((currentYear + 2) % 4 === 0) {
-  leap.innerHTML = `El próximo año bisiesto es el ${currentYear + 2}`;
+  leap.innerHTML = `El próximo año bisiesto es el ${currentYear + 2}.`;
 } else if ((currentYear + 3) % 4 === 0) {
-  leap.innerHTML = `El próximo año bisiesto es el ${currentYear + 3}`;
+  leap.innerHTML = `El próximo año bisiesto es el ${currentYear + 3}.`;
 }
