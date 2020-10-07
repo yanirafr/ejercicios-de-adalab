621be22004bc7a1c48980790e949b2e7e632f036
Yanira <yanirafr@gmail.com>
2020-10-07 21:04:43 +0200

    upload

---
+++ 2.6/2.6-2/main.js
@@ -0,0 +1,9 @@
+const adalaber1 = {
+    name: "Susana",
+    age: 34,
+    job: "periodista",
+    run: "Estoy corriendo",
+    runMarathon: km => `Estoy corriendo una maratón de ${km} metros.`
+};
+console.log(adalaber1.run);
+console.log(adalaber1.runMarathon("50"));
