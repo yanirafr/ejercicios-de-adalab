5e01553d86162659f7f1525ddd96f25f77c2903f
Yanira <yanirafr@gmail.com>
2020-10-09 16:49:54 +0200

    improve code

--- 2.7/2.7-5/main.js
+++ 2.7/2.7-5/main.js
@@ -1,8 +1,19 @@
-const numbers = [5, 10, 7, 3, 8];
-let mean = 0;
 
-for (let i = 0; i < numbers.length; i++){
-    mean += numbers[i]/numbers.length;
+function avg (numbers) {
+    // const numbers = [5, 10, 7, 3, 8];
+    let mean = 0;
+    for (let i = 0; i < numbers.length; i++){
+        mean += numbers[i]/numbers.length;
+    };
+    return mean;
 }
 
-console.log(mean);
+console.log(avg([5, 10, 7, 3, 8]));
+console.log(avg([1, 2, 3]));
+
+const avgResult = avg([1, 2, 3]);
+console.log(avgResult)
+
+const myNumbers = [1, 2, 3];
+const avgResult2 = avg(myNumbers);
+console.log(avgResult2)
--- 2.7/2.7-6/main.js
+++ 2.7/2.7-6/main.js
@@ -1,14 +1,30 @@
-let fav1 = document.querySelector(".fav1").value;
-let fav2 = document.querySelector(".fav2").value;
-const send = document.querySelector(".send");
+/*const send = document.querySelector(".send");
 const answer = document.querySelector(".answer");
 
-movies = [fav1, fav2];
-
 function askMovie () {
+    let fav1 = document.querySelector(".fav1").value;
+    let fav2 = document.querySelector(".fav2").value;
+    const movies = [fav1, fav2];
+    console.log(movies)
     for (const favourite of movies) {
         console.log(`¡A mí también me encantó ${favourite}! Tenemos mucho en común, humana.`);
     }
 }
 
-send.addEventListener("click", askMovie)
+send.addEventListener("click", askMovie);*/
+
+let fav1 = document.querySelector(".fav1");
+let fav2 = document.querySelector(".fav2");
+const send = document.querySelector(".send");
+const answer = document.querySelector(".answer");
+
+function askMovie () {
+    const fav1Value = fav1.value;
+    const movies = [fav1Value, fav2.value];
+    console.log(movies)
+    for (const favourite of movies) {
+        console.log(`¡A mí también me encantó ${favourite}! Tenemos mucho en común, humana.`);
+    }
+}
+
+send.addEventListener("click", askMovie);
