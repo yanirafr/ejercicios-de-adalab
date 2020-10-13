1d1b353dc4fe117fb67113249390538908d21021
Yanira <yanirafr@gmail.com>
2020-10-13 11:38:13 +0200

    upload

--- 2.7/2.7-5/main.js
+++ 2.7/2.7-5/main.js
@@ -1,19 +1,18 @@
-
-function avg (numbers) {
-    // const numbers = [5, 10, 7, 3, 8];
-    let mean = 0;
-    for (let i = 0; i < numbers.length; i++){
-        mean += numbers[i]/numbers.length;
-    };
-    return mean;
+function avg(numbers) {
+  // const numbers = [5, 10, 7, 3, 8];
+  let mean = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    mean += numbers[i] / numbers.length;
+  }
+  return mean;
 }
 
 console.log(avg([5, 10, 7, 3, 8]));
 console.log(avg([1, 2, 3]));
 
 const avgResult = avg([1, 2, 3]);
-console.log(avgResult)
+console.log(avgResult);
 
 const myNumbers = [1, 2, 3];
 const avgResult2 = avg(myNumbers);
-console.log(avgResult2)
+console.log(avgResult2);
