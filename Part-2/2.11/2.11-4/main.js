2f40b06b8cd43438f6ed471bc118424659dd2c7e
Yanira <yanirafr@gmail.com>
2020-10-19 11:48:01 +0200

    indent

--- Part-2/2.11/2.11-4/main.js
+++ Part-2/2.11/2.11-4/main.js
@@ -1,22 +1,16 @@
 const input = document.querySelector(".name");
 const write = document.querySelector(".write");
 
-function getName () {
-    
-    write.innerHTML = input.value;
-
-    const name = localStorage.setItem("name", input.value);
-
+function getName() {
+  write.innerHTML = input.value;
+  const name = localStorage.setItem("name", input.value);
 }
 
 input.addEventListener("keyup", getName);
 
-function reset () {
-
-    const resetName = localStorage.getItem("name");
-
-    write.innerHTML = resetName;
-
+function reset() {
+  const resetName = localStorage.getItem("name");
+  write.innerHTML = resetName;
 }
 
 reset();
