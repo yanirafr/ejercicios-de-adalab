9aaf6401c96974809dfbd292a04ed368c97c5b67
Yanira <yanirafr@gmail.com>
2020-11-02 17:42:46 +0100

    add extend function

--- Module-3/3.1/3.2/3.2-1/src/App.js
+++ Module-3/3.1/3.2/3.2-1/src/App.js
@@ -1,41 +1,41 @@
-import catrina from "./Katrina.png";
+import React, { Component } from "react";
+import catrina from "./catrina.png";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={catrina} className="App-img" alt="logo" title="Photo" />
-        <div className="header__box">
-          <h1 className="App-name">Yanira Fernández</h1>
-          <h2 className="App-date">Lunes 02 de noviembre de 2020</h2>
-        </div>
-      </header>
-      <main className="App-main">
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
-          laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
-          doloremque, officia ea qui porro vel possimus quo explicabo veniam
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
-          laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
-          doloremque, officia ea qui porro vel possimus quo explicabo veniam
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
-          laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
-          doloremque, officia ea qui porro vel possimus quo explicabo veniam
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
-          laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
-          doloremque, officia ea qui porro vel possimus quo explicabo veniam
-        </p>
-      </main>
-      <footer className="App-footer">
-        <small className="App-small">Leer más...</small>
-        <div className="App-likes">
-          <p>37</p>
-          <img src={catrina} className="App-heart" alt="heart" title="heart" />
-        </div>
-      </footer>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={catrina} className="App-img" alt="logo" title="Photo" />
+          <div className="header__box">
+            <h1 className="App-name">Yanira Fernández</h1>
+            <h2 className="App-date">Lunes 02 de noviembre de 2020</h2>
+          </div>
+        </header>
+        <main className="App-main">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
+            laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
+            doloremque, officia ea qui porro vel possimus quo explicabo veniam
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
+            laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
+            doloremque, officia ea qui porro vel possimus quo explicabo veniam
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
+            laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
+            doloremque, officia ea qui porro vel possimus quo explicabo veniam
+          </p>
+        </main>
+        <footer className="App-footer">
+          <small className="App-small">Leer más...</small>
+          <div className="App-likes">
+            <p>37</p>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
