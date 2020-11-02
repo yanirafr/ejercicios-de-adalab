b23d6eb794a506786959f49bf3c628323480fbb6
Yanira <yanirafr@gmail.com>
2020-11-02 20:00:59 +0100

    add part two of the exercise

--- Module-3/3.1/3.2/3.2-1/src/App.js
+++ Module-3/3.1/3.2/3.2-1/src/App.js
@@ -4,37 +4,47 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return (
+    const appHeader = (
+      <header className="App-header">
+        <img src={catrina} className="App-img" alt="logo" title="Photo" />
+        <div className="header__box">
+          <h1 className="App-name">Yanira Fernández</h1>
+          <h2 className="App-date">Lunes 02 de noviembre de 2020</h2>
+        </div>
+      </header>
+    );
+    const appMain = (
+      <main className="App-main">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
+          laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
+          doloremque, officia ea qui porro vel possimus quo explicabo veniam
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
+          laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
+          doloremque, officia ea qui porro vel possimus quo explicabo veniam
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
+          laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
+          doloremque, officia ea qui porro vel possimus quo explicabo veniam
+        </p>
+      </main>
+    );
+    const appFooter = (
+      <footer className="App-footer">
+        <small className="App-small">Leer más...</small>
+        <div className="App-likes">
+          <p>37</p>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    );
+    const appBox = (
       <div className="App">
-        <header className="App-header">
-          <img src={catrina} className="App-img" alt="logo" title="Photo" />
-          <div className="header__box">
-            <h1 className="App-name">Yanira Fernández</h1>
-            <h2 className="App-date">Lunes 02 de noviembre de 2020</h2>
-          </div>
-        </header>
-        <main className="App-main">
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
-            laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
-            doloremque, officia ea qui porro vel possimus quo explicabo veniam
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
-            laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
-            doloremque, officia ea qui porro vel possimus quo explicabo veniam
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
-            laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
-            doloremque, officia ea qui porro vel possimus quo explicabo veniam
-          </p>
-        </main>
-        <footer className="App-footer">
-          <small className="App-small">Leer más...</small>
-          <div className="App-likes">
-            <p>37</p>
-            <i class="fas fa-heart"></i>
-          </div>
-        </footer>
+        {appHeader}
+        {appMain}
+        {appFooter}
       </div>
     );
+    return appBox;
   }
 }
 
