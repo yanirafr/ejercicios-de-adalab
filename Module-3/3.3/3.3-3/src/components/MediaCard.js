a8ff36d09bbeb3092207ccd11e53c63ec7bf7e35
Yanira <yanirafr@gmail.com>
2020-11-03 23:18:49 +0100

    convert Mediacard to functional

--- Module-3/3.3/3.3-3/src/components/MediaCard.js
+++ Module-3/3.3/3.3-3/src/components/MediaCard.js
@@ -2,53 +2,35 @@ import React, { Component } from "react";
 import logo from "../catrina.png";
 import "../App.scss";
 
-class MediaCard extends Component {
-  render() {
-    const userName = "Yanira Fernández";
-    const date = "Lunes 02 de noviembre de 2020";
-    const text = ` Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
-    laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
-    doloremque, officia ea qui porro vel possimus quo explicabo veniam
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
-    laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
-    doloremque, officia ea qui porro vel possimus quo explicabo veniam
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos,
-    laudantium, repudiandae aspernatur esse natus saepe at beatae ipsa
-    doloremque, officia ea qui porro vel possimus quo explicabo veniam`;
-    const likes = "02";
-
-    const appHeader = (
+const MediaCard = (props) => {
+  return (
+    <div className="box">
       <header className="header">
-        <img src={logo} className="header__img" alt="logo" title="Photo" />
+        <img
+          src={props.photo}
+          className="header__img"
+          alt="Photo"
+          title="Photo"
+        />
         <div className="header__box">
-          <h1 className="header__name">{userName}</h1>
-          <h2 className="header__date">{date}</h2>
+          <h1 className="header__name">{props.name}</h1>
+          <h2 className="header__date">{props.date}</h2>
         </div>
       </header>
-    );
-    const appMain = (
+
       <main className="main">
-        <p>{text}</p>
+        <p>{props.text}</p>
       </main>
-    );
-    const appFooter = (
+
       <footer className="footer">
         <small className="footer__small">Leer más...</small>
         <div className="footer__likes">
-          <p>{likes}</p>
+          <p>{props.likes}</p>
           <i class="footer__icon fas fa-heart"></i>
         </div>
       </footer>
-    );
-    const appBox = (
-      <div className="box">
-        {appHeader}
-        {appMain}
-        {appFooter}
-      </div>
-    );
-    return appBox;
-  }
-}
+    </div>
+  );
+};
 
 export default MediaCard;
