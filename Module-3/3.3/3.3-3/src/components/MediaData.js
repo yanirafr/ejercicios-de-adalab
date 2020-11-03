7c0751a8f39099df09c38ad9b3a5c9173e0b3749
Yanira <yanirafr@gmail.com>
2020-11-03 23:06:53 +0100

    upload

---
+++ Module-3/3.3/3.3-3/src/components/MediaData.js
@@ -0,0 +1,19 @@
+// import React, { Component } from "react";
+// import MediaCard from "./MediaCard";
+// import img from "../catrinna.png";
+
+// class MediaData extends Component {
+//   render() {
+//     return (
+//       <MediaCard
+//         name="Yanira"
+//         date="03 de noviembre de 2020"
+//         image={img}
+//         text="Lorem"
+//         likes="2"
+//       />
+//     );
+//   }
+// }
+
+// export default MediaData;
