898c7f8e51d27a2bf97f278b8b2e36741e3441c2
Yanira <yanirafr@gmail.com>
2020-11-03 21:42:44 +0100

    upload

---
+++ Module-3/3.3/Ex/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import MediaCard from "./MediaCard";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <MediaCard />
+      </>
+    );
+  }
+}
+
+export default App;
