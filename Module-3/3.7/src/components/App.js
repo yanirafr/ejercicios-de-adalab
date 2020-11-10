d25622431754decb9b65b8626bc1048cc724da7f
Yanira <yanirafr@gmail.com>
2020-11-10 23:56:13 +0100

    upload

---
+++ Module-3/3.7/src/components/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [year, getYear] = useState(32);
+  const [gift, getGift] = useState(1);
+
+  const getOlder = () => {
+    getYear(year + 1);
+  };
+  const getPresents = () => {
+    getGift(gift + 1);
+  };
+
+  return (
+    <div>
+      <p className="years">Tengo {year} años}</p>
+      <p className="gifts">Tengo {gift} regalos</p>
+      <button onClick={getOlder}>Hazme más vieja</button>
+      <button onClick={getPresents}>Dame más regalos</button>
+    </div>
+  );
+};
+
+export default App;
